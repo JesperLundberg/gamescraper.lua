17089cd86b74484776eff34441e423fbc00e1713
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-08-13 11:06:26 +0200

    Add database beginning

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 local steam_api = require("steam_api")
+local database = require("database")
 
 local raw_json = steam_api.get_raw_data_2_weeks()
 
-print("From main.lua: " .. raw_json)
+-- print("From main.lua: " .. raw_json)
+
+database.setup()
