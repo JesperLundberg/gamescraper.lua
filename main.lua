6952c333cb36853fb5c8e55a2a233e36f3054eff
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-08-19 12:48:49 +0200

    Refactor raw data code into logic folder as well

--- main.lua
+++ main.lua
@@ -1,15 +1,11 @@
-local steam_api = require("http.steam_api")
-local database_raw_data = require("database.raw_data")
+local raw_layer = require("logic.create_raw_data_layer")
 local bronze_layer = require("logic.create_bronze_layer")
-
--- Get the raw json data from the Steam API
-local raw_json = steam_api.get_raw_data_2_weeks()
 
 -- Get todays date in string format
 local date = os.date("%Y-%m-%d")
 
--- Insert the raw json data into the database
-database_raw_data.insert_raw_data(date, raw_json)
+-- Create the raw data layer for todays date
+raw_layer.create_raw_data_layer(date)
 
 -- Create the bronze layer for todays date
 bronze_layer.create_bronze_layer(date)
