c1d0f7feaded84b9b426e63ab4aa9cc8dba5d22a
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-08-21 19:02:28 +0200

    Add luals annotations for configuration

--- config.lua
+++ config.lua
@@ -1,3 +1,9 @@
+--- This file should be used to store local configuration settings that are specific to your environment.
+--- @class configuration
+--- @field steam_api_key string Your Steam API key
+--- @field steam_user_id string Your Steam user ID
+--- @field database_path string The path to the SQLite database file
+--- @field report_path string The path to where the reports should be stored
 local M = {}
 
 M.steam_api_key = "YOUR_STEAM_API_KEY"
--- utils.lua
+++ utils.lua
@@ -55,6 +55,7 @@ function M.flatten_gamedata(tab)
 end
 
 --- Get the configuration from the config_local.lua file if it exists, otherwise get the configuration from the config.lua file
+--- @return configuration|LazyCoreConfig
 function M.get_config()
 	-- Check if the config_local.lua file exists
 	local status, config = pcall(require, "config_local")
