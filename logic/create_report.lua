33eb559c9a5521376e26a33f6b3f40ead4bd7dc2
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-08-31 22:20:32 +0200

    Fix linting errors

--- logic/create_report.lua
+++ logic/create_report.lua
@@ -1,10 +1,10 @@
 local M = {}
 
-local bronze_layer_database = require("database.bronze_layer")
+-- local bronze_layer_database = require("database.bronze_layer")
 
 function M.CreateReport()
 	-- Get the data from the bronze layer
-	local data = bronze_layer_database.get_bronze_layer()
+	-- local data = bronze_layer_database.get_bronze_layer()
 
 	-- Switch on what type of report to create?
 
