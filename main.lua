b87a8cf8dd8cd5b8bc90f52e570a00d4633b6c10
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-08-31 22:10:08 +0200

    Run the previous data before running todays data

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 local raw_layer = require("logic.create_raw_data_layer")
 local report_layer = require("logic.create_report_layer")
 
+-- Create the report layer for all dates between the last run date and today
+report_layer.create_report_data_to_today()
+
 -- Get todays date in string format
 local date = os.date("%Y-%m-%d")
 
@@ -11,5 +14,3 @@ raw_layer.create_raw_data_layer(date)
 report_layer.create_report_layer(date)
 
 print("Done!")
-
-report_layer.create_report_data_to_today()
