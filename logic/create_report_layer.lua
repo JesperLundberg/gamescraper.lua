0176b6292b97910e26a6da918cbf5ec12cae94be
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-09-01 22:11:07 +0200

    First test successfully created

--- logic/create_report_layer.lua
+++ logic/create_report_layer.lua
@@ -10,9 +10,9 @@ function M.create_report_layer(date)
 	-- Get the raw data by sent in date or todays date
 	local raw_data = database_raw_data.get_raw_data_by_date(date or os.date("%Y-%m-%d"))
 
-	-- If there is no raw data, return
+	-- If there is no raw data, return nil to indicate that nothing was done
 	if not next(raw_data) then
-		return
+		return nil
 	end
 
 	-- Make the raw json into a table
--- tests/create_report_layer_spec.lua
+++ tests/create_report_layer_spec.lua
@@ -1,2 +1,32 @@
-local mock = require("busted.mock")
-local assert = require("luassert")
+local busted = require("busted")
+local describe, it, assert, stub, before_each, after_each =
+	busted.describe, busted.it, busted.assert, busted.stub, busted.before_each, busted.after_each
+
+-- System under test
+local sut = require("logic.create_report_layer")
+
+-- Get necessary modules in order to stub them
+local database_raw_data = require("database.raw_data")
+
+describe("create_report_layer", function()
+	-- Stubs
+	local database_raw_data_get_raw_data_by_date_stub
+
+	before_each(function()
+		database_raw_data_get_raw_data_by_date_stub = stub(database_raw_data, "get_raw_data_by_date")
+	end)
+
+	after_each(function()
+		database_raw_data_get_raw_data_by_date_stub:revert()
+	end)
+
+	describe("when called with a valid date", function()
+		it("it should return nil if there is no raw data for that date", function()
+			database_raw_data_get_raw_data_by_date_stub.returns({})
+
+			local result = sut.create_report_layer("2021-01-01")
+
+			assert.is_nil(result)
+		end)
+	end)
+end)
